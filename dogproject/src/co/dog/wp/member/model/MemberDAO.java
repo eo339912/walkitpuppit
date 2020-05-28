8a64691d719fce922ab427dd9bb889e920aaffb1
eo339912 <eo339912@example.com>
2020-05-28 15:02:31 +0900

    0528

---
+++ dogproject/src/co/dog/wp/member/model/MemberDAO.java
@@ -0,0 +1,181 @@
+package co.dog.wp.member.model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+import co.dog.wp.common.ConnectionManager;
+
+public class MemberDAO {
+	Connection conn = null;
+	PreparedStatement psmt = null;
+	ResultSet rs = null;
+
+	// 등록
+	public int memberInsert(MemberVO member) {
+		int r = 0;
+
+		try {
+			// 1. DB 연결
+			conn = ConnectionManager.getConnnect();
+
+			// 2. sql구문 준비
+			String sql = "insert into member (seq, id, pwd, name, pbirth, pgender, pimage, pname, regdt)"
+					+ " values ( seq_member.nextval, ?, ?,?, TO_DATE(?, 'yyyymmdd'), ?, ?, ?, sysdate)";
+
+			psmt = conn.prepareStatement(sql);
+
+			// 3. 실행
+			psmt.setString(1, member.getId());
+			psmt.setString(2, member.getPwd());
+			psmt.setString(3, member.getName());
+			psmt.setString(4, member.getPbirth());
+			psmt.setString(5, member.getPgender());
+			psmt.setString(6, member.getPimage());
+			psmt.setString(7, member.getPname());
+
+			r = psmt.executeUpdate();
+
+			// 4. 결과처리
+			System.out.println(r + " 건이 등록됨.");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			// 5. 연결해제
+			ConnectionManager.close(conn);
+		}
+
+		return r;
+	}
+
+	// 한건조회
+	public MemberVO getMember(String id) {
+		MemberVO vo = new MemberVO();
+
+		try {
+			// 1. DB연결
+			conn = ConnectionManager.getConnnect();
+			// 2. 쿼리준비
+			String sql = "select * from member where id=?";
+			psmt = conn.prepareStatement(sql);
+			// 3. statement 실행
+			psmt.setString(1, id);
+			ResultSet rs = psmt.executeQuery();
+			if (rs.next()) {
+				vo.setId(rs.getString("id"));
+				vo.setName(rs.getString("name"));
+				vo.setPwd(rs.getString("pwd"));
+				vo.setRegdt(rs.getString("regdt"));
+			}
+			// 4. 결과저장
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			// 5. DB연결 해제
+			ConnectionManager.close(conn);
+		}
+
+		return vo;
+	}
+
+	// 수정
+	public int memberUpdate(MemberVO member) {
+		int r = 0;
+
+		try {
+			// 1. DB 연결
+			conn = ConnectionManager.getConnnect();
+
+			// 2. sql구문 준비
+			String sql = "update member set pwd=?, name=?, hobby=?, gender=?, religion=?, introduction=?"
+					+ " where id=?";
+
+			psmt = conn.prepareStatement(sql);
+
+			// 3. 실행
+			psmt.setString(1, member.getPwd());
+			psmt.setString(2, member.getName());
+			psmt.setString(7, member.getId());
+
+			r = psmt.executeUpdate();
+
+			// 4. 결과처리
+			System.out.println(r + " 건이 수정됨.");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			// 5. 연결해제
+			ConnectionManager.close(conn);
+		}
+
+		return r;
+	}
+
+	// 전체조회
+	public ArrayList<MemberVO> getMemberList() {
+		ArrayList<MemberVO> list = new ArrayList<MemberVO>();
+
+		try {
+			// 1. DB연결
+			conn = ConnectionManager.getConnnect();
+			// 2. 쿼리준비
+			String sql = "select * from member order by id";
+			psmt = conn.prepareStatement(sql);
+			// 3. statement 실행
+			ResultSet rs = psmt.executeQuery();
+			while (rs.next()) {
+				MemberVO vo = new MemberVO();
+				vo.setId(rs.getString("id"));
+				vo.setPwd(rs.getString("pwd"));
+				vo.setRegdt(rs.getString("regdt"));
+
+				list.add(vo);
+			}
+			// 4. 결과저장
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			// 5. DB연결 해제
+			ConnectionManager.close(conn);
+		}
+
+		return list;
+	}
+
+	// 삭제
+	public int memberDelete(MemberVO member) {
+		int r = 0;
+
+		try {
+			// 1. DB 연결
+			conn = ConnectionManager.getConnnect();
+
+			// 2. sql구문 준비
+			String sql = "delete from member where id= ? ";
+
+			psmt = conn.prepareStatement(sql);
+
+			// 3. 실행
+			psmt.setString(1, member.getId());
+
+			r = psmt.executeUpdate();
+
+			// 4. 결과처리
+			System.out.println(r + " 건이 수정됨.");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			// 5. 연결해제
+			ConnectionManager.close(conn);
+		}
+
+		return r;
+	}
+
+}
