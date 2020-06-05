ca79a2451b51875a83ca95af4880cda346c4b398
riswell79 <riswell79@example.com>
2020-06-05 09:15:52 +0900

    y

--- dogproject/docu/ddl_jw.sql
+++ dogproject/docu/ddl_jw.sql
@@ -89,6 +89,6 @@ FROM park p
 INNER JOIN walk w ON p.seq = w.p_seq where p.seq = 205 and w.incheck = 1
 
 select * from walk where p_seq = 205 and incheck = 1;
-h
+
 select * from message;
 select * from message where m_id = 'admin' order by seq desc;
